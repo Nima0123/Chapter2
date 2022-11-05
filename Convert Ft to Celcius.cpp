21731d86ddb1f404ee4700f2f0352da57585fcbe
Nima <116268126+Nima0123@users.noreply.github.com>
2022-11-05 15:32:59 +0330

    Update Convert Ft to Celcius.cpp

--- Convert Ft to Celcius.cpp
+++ Convert Ft to Celcius.cpp
@@ -5,6 +5,6 @@ int main(){
     int Fahrenheit;
     cout<<"Enter the Fahrenheit:";
     cin>>Fahrenheit;
-    cout<<"Celsius is:"<<5/9*(Fahrenheit-32);
+    cout<<"Celsius is:"<<(5/9)*(Fahrenheit-32);
     return 0;
 }
